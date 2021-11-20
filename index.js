2180b7c778230f3cf8c6cb60c0b0006bddcd99fc
satryanararya <b.satrya31@gmail.com>
2021-11-20 17:15:19 +0800

    testing pertama

---
+++ index.js
@@ -0,0 +1,8 @@
+//const = konstanta, mutlak (gabisa diganti)
+const nama = "satrya";
+//let, fleksibel (bisa diganti)
+let hobi = "gaming";
+    hobi = "maen sepak takraw" //contoh untuk ngeganti yang 'gaming'
+
+//buat manggil variabel
+console.log(`${nama} ${hobi}`);
