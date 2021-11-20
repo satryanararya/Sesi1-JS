76864e982b4965fbca12fd2650b1ff2c53c0818c
satryanararya <b.satrya31@gmail.com>
2021-11-20 17:43:41 +0800

    perubahan kedua

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 //const = konstanta, mutlak (gabisa diganti)
 const nama = "satrya";
 //let, fleksibel (bisa diganti)
-let hobi = "gaming";
-    hobi = "maen sepak takraw" //contoh untuk ngeganti yang 'gaming'
 
-//buat manggil variabel
-console.log(`${nama} ${hobi}`);
+function getNama() {
+    return nama;
+}
+console.log(getNama());
+
